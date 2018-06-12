fe997dd591a87a273ca6afa2ce11202b966b5914
Jason Mitchell <jason.mitchell.w@gmail.com>
2018-06-11 21:15:29 -0700

    Updating test to use waitFor and increase timeout

--- tests/acceptance/4-case-loading-test.js
+++ tests/acceptance/4-case-loading-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, waitUntil } from '@ember/test-helpers';
+import { visit, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupQunit as setupPolly } from '@pollyjs/core';
 
@@ -14,12 +14,12 @@ module('Acceptance | Class | Loading Substate', function(hooks) {
       .get(
         'https://ember-api-docs.global.ssl.fastly.net/json-docs/ember/1.0.0/*path'
       )
-      .on('beforeResponse', (req, res) => server.timeout(2000));
+      .on('beforeResponse', (req, res) => server.timeout(3000));
   });
 
   test('loads the loading substate', async function(assert) {
     const promise = visit('/ember/1.0/classes/Container');
-    await waitUntil(() => document.querySelector('.loading-spinner'))
+    await waitFor('.loading-spinner');
     assert.dom('.loading-spinner').exists();
 
     await promise;
